406b8d48ddd54b6bb3ad4c03d57f423e571ed1cf
dreamlike_ocean <dreamlike.vertx@gmail.com>
2024-06-25 00:09:36 +0800

    1,init commit

--- leyden-simple/src/main/java/io/github/dreamlike/Main.java
+++ leyden-simple/src/main/java/io/github/dreamlike/Main.java
@@ -15,27 +15,3 @@ public class Main {
        new HeavyClassInitCase();
     }
 }
-
-//var rootPath = Path.of("/home/dreamlike/jdks/jdk/src/java.base/share/classes");
-//        ArrayList<String> classNames = new ArrayList<>();
-//        ArrayDeque<Path> paths = new ArrayDeque<>();
-//        paths.add(rootPath);
-//        while (!paths.isEmpty()) {
-//            var path = paths.pop();
-//            var file = path.toFile();
-//            if (file.isDirectory()) {
-//                var files = file.listFiles();
-//                for (var f : files) {
-//                    paths.add(f.toPath());
-//                }
-//            } else {
-//                var name = file.getName();
-//                if (name.endsWith(".java") && !name.contains("module-info") && !name.contains("package-info")) {
-//                    var className = path.toString().substring(rootPath.toString().length() + 1);
-//                    className = className.substring(0, className.length() - 5).replace('/', '.');
-//                    classNames.add(className);
-//                }
-//            }
-//        }
-//        String all = String.join("\n", classNames);
-//        Files.writeString(Path.of("all.txt"), all);
